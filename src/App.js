1bf1198ef6dbbabde8bb37f5cbaa750280f2b7f4
sravya <kandukurisravya0306@gmail.com>
2023-05-22 10:58:52 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Heading, CustomButton} from './styledComponents'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <Heading>Hello World</Heading>
+    <CustomButton type="button">Click</CustomButton>
+    <CustomButton type="button" outline>
+      Click
+    </CustomButton>
+  </>
+)
+
+export default App
